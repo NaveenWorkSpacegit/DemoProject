b9280444738f792660d7cbcbc26939155cd2c6b6
Prasanna Naveen <pranavee@publicisgroupe.net>
2022-04-10 17:59:13 +0530

    First commit

---
+++ add.java
@@ -0,0 +1,11 @@
+import javax.print.event.PrintEvent;
+import javax.sql.rowset.spi.SyncResolver;
+import javax.swing.text.PlainDocument;
+
+public class add {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a+b);
+    }
+}
