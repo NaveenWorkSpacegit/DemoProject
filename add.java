f1afe4de1ef119a09b24a5f727cf37a1e3ff3c25
NaveenWorkSpacegit <101691470+NaveenWorkSpacegit@users.noreply.github.com>
2022-04-10 18:23:02 +0530

    Changed variable a to 100

--- add.java
+++ add.java
@@ -4,7 +4,7 @@ import javax.swing.text.PlainDocument;
 
 public class add {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 100;
         int b = 2;
         System.out.println(a+b);
     }
