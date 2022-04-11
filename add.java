b7e105b32df8cba2c4ad35c13885195bb82164f4
Prasanna Naveen <pranavee@publicisgroupe.net>
2022-04-11 08:01:52 +0530

    changed a to 5000

--- add.java
+++ add.java
@@ -1,10 +1,6 @@
-import javax.print.event.PrintEvent;
-import javax.sql.rowset.spi.SyncResolver;
-import javax.swing.text.PlainDocument;
-
 public class add {
     public static void main(String[] args) {
-        int a = 200;
+        int a = 5000;
         int b = 2;
         System.out.println(a+b);
     }
