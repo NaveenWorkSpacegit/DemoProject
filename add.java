a8fb325c6be830affa6a5df80c7e7d35e53ad79d
Prasanna Naveen <pranavee@publicisgroupe.net>
2022-04-11 07:53:32 +0530

    Switched to multiply

--- add.java
+++ add.java
@@ -1,11 +1,7 @@
-import javax.print.event.PrintEvent;
-import javax.sql.rowset.spi.SyncResolver;
-import javax.swing.text.PlainDocument;
-
 public class add {
     public static void main(String[] args) {
         int a = 200;
         int b = 2;
-        System.out.println(a+b);
+        System.out.println(a*b);
     }
 }
